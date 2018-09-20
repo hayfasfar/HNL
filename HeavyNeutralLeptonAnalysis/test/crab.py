9d406cd6d3017a3d6d974ffa4992e3d4361c8a5a
Haifa Rejeb Sfar <haifa.rejeb.sfar@cern.ch>
2018-09-20 12:14:26 +0200

    new files  for crab submission

---
+++ HeavyNeutralLeptonAnalysis/test/crab.py
@@ -0,0 +1,42 @@
+from WMCore.Configuration import Configuration
+import os
+
+# Get parameters from submit script
+productionLabel = os.environ['CRAB_PRODUCTIONLABEL']
+dataset         = os.environ['CRAB_DATASET']
+outputFile      = os.environ['CRAB_OUTPUTFILE']
+lumiMask        = os.environ['CRAB_LUMIMASK']
+
+requestName = dataset.split('/')[2] + '_' + productionLabel
+requestName = requestName.replace('RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6','Moriond2017')
+requestName = requestName.replace('RunIISummer16MiniAODv2-PUMoriond17_HCALDebug_80X_mcRun2_asymptotic_2016_TrancheIV_v6','Moriond2017')
+requestName = requestName.replace('RunIISummer16MiniAODv2-80X_mcRun2_asymptotic_2016_TrancheIV_v6', '80X')
+requestName = requestName.replace('RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016', 'Spring16FS')
+requestName = requestName.replace('RunIISummer16MiniAODv2-PUSummer16Fast_80X_mcRun2_asymptotic_2016_TrancheIV_v6', 'Moriond2017FS')
+
+# Crab configuration
+config = Configuration()
+config.section_('General')
+config.General.transferLogs            = True
+config.General.requestName             = requestName
+config.General.workArea                = os.path.join('crabAlpha', productionLabel, dataset.split('/')[1])
+
+config.section_('JobType')
+config.JobType.psetName                = 'HeavyNeutralLeptonAnalyzer_cfg.py'
+config.JobType.pyCfgParams             = ['events=-1', 'outputFile='+outputFile, 'inputFile='+dataset]
+config.JobType.pluginName              = 'analysis'
+config.JobType.outputFiles             = [outputFile]
+config.JobType.sendExternalFolder      = True
+config.JobType.allowUndistributedCMSSW = True 
+
+config.section_('Data')
+config.Data.inputDataset               = dataset
+config.Data.unitsPerJob                = 300 if 'SIM' in dataset else 50
+config.Data.splitting                  = 'LumiBased'
+config.Data.outLFNDirBase              = '/store/user/' + os.environ['USER'] + '/data2016/'
+config.Data.publication                = False
+config.Data.lumiMask                   = lumiMask if not 'SIM' in dataset else None
+config.Data.allowNonValidInputDataset = True #allow unfinished samples (production) to be processed
+
+config.section_('Site')
+config.Site.storageSite                = 'T2_BE_IIHE'
---
+++ HeavyNeutralLeptonAnalysis/test/submitAll.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os, glob, sys
+#import function returning JSON from multilep.py
+from HeavyNeutralLeptonAnalyzer_cfg import getJSON
+
+datasetsFile    = sys.argv[1]                                                                             # Input file with datasets
+productionLabel = os.path.basename(datasetsFile.split('/')[-1].split('.')[0])                             # Label to keep track of the tuple version (is taken from the name of the above input file)
+outDir          = '/user/' + os.environ['USER'] + '/public/heavyNeutrino'                                 # Output directory in case of local submission
+datasets        = [dataset.strip() for dataset in open(datasetsFile)]                                     # Get list of datasets from file given as first argument
+datasets        = [dataset.split()[0] for dataset in datasets if dataset and not dataset.startswith('#')] # Clean empty and comment lines
+#check if call asked for local submission
+submitLocal     = ""
+#Use third argument to specify the number of jobs per file
+filesPerJob     = 10
+
+if len(sys.argv) > 2:
+    submitLocal = sys.argv[2]
+    if len(sys.argv) > 3:
+        filesPerJob = sys.argv[3]
+
+for dataset in datasets:
+    skim, dataset = dataset.split(':')
+
+    if 'pnfs' in dataset or 'user' in dataset or submitLocal == "local":
+        dir        = os.getcwd()
+        outputDir = outDir + "/"
+        if 'pnfs' in dataset and 'user' in dataset: outputDir = outputDir + dataset.split('/')[-2] + '/' + dataset.split('/')[-1]      
+        else: 
+	    print 'outputDir: ' + outputDir
+	    print 'dataset.split: ' + dataset.split('/')[1]
+	    outputDir = outputDir + dataset.split('/')[1]
+        if 'ext' in dataset:
+            outputDir = outputDir + "_"
+            index = dataset.find('ext')
+            for i in range (index, len(dataset)):
+                if dataset[i] == "/": break
+                else : outputDir = outputDir + dataset[i]
+        #add run labels to data output directory 
+        if ('Run2016' in dataset) or ('Run2017' in dataset):
+            if('Run2016') in dataset :
+                outputDir = outputDir + "/Run2016"
+            else :
+                outputDir = outputDir + "/Run2017"
+            for era in ["B", "C", "D", "E", "F", "G", "H"]:
+                if ( ("Run2016" + era) in dataset ) or ( ("Run2017" + era) in dataset ): 
+                    outputDir = outputDir + era
+
+
+        print outputDir
+        #cut out the first part of /pnfs path for official sample if needed
+        if 'pnfs' in dataset and 'user' not in dataset:
+            dataset = dataset.replace("/pnfs/iihe/cms/ph/sc4/store/mc", "")
+            #naming in pnfs directories is slightly altered compared to the CMSDAS name, rever this to the CMSDAS version:
+            period = dataset[1:].split('/')[0]
+            name   = dataset[1:].split('/')[1]
+            form   = dataset[1:].split('/')[2]
+            puScen = dataset[1:].split('/')[3]
+            dataset = '/' + name + '/' + period + '-' + puScen + '/' + form
+	    outputDir = outputDir + name
+	    print 'outputdirectory: ' + outputDir
+
+        os.system('bash runLocal.sh ' + dataset + ' ' + outputDir + ' ' + skim + ' ' + str(filesPerJob) )
+
+    else: # use crab
+        print 'Submitting ' + dataset + ' using crab:'
+        os.environ['CRAB_PRODUCTIONLABEL'] = productionLabel
+        os.environ['CRAB_DATASET']         = dataset
+        os.environ['CRAB_OUTPUTFILE']      = skim + '.root'
+        #os.environ['CRAB_OUTPUTFILE']      = 'data2016.root'
+        if 'Run2017' in dataset : os.environ['CRAB_LUMIMASK'] = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/PromptReco/" + getJSON( ('Run2017' in dataset) )
+        else : os.environ['CRAB_LUMIMASK'] = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/ReReco/Final/" + getJSON( ('Run2017' in dataset) )
+        os.system('crab submit -c crab.py')
